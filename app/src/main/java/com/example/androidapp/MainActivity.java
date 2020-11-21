7d825a3fd8f53d8bc9abc377ffb8b5cc334c0917
tomsbaugis <toms.baugis123@gmail.com>
2020-11-21 13:10:19 +0200

    Toms Bauģis pd1

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.androidapp;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView groupName = (TextView)findViewById(R.id.groupName1);
+        final TextView participant1 = (TextView)findViewById(R.id.participant1);
+        final TextView participant2 = (TextView)findViewById(R.id.participant2);
+        final TextView participant3 = (TextView)findViewById(R.id.participant3);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                groupName.setText("Group number: 4");
+                participant1.setText("First participant: Toms Bauģis");
+                participant2.setText("Second participant: Kristers Vēveris");
+                participant3.setText("Third participant: Artis Tauriņš");
+            }
+        });
     }
 }
