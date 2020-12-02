90c684a72faf0c65cb85f8781f96aa01e5e1e9e9
tomsbaugis <toms.baugis123@gmail.com>
2020-12-02 22:48:34 +0200

    Fixed dialog "OK" button

--- app/src/main/java/com/example/androidapp/Activity2.java
+++ app/src/main/java/com/example/androidapp/Activity2.java
@@ -7,13 +7,12 @@ import android.os.Bundle;
 
 public class Activity2 extends AppCompatActivity {
 
-    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-        button = (Button) findViewById(R.id.buttonBack);
+        Button button = findViewById(R.id.buttonBack);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/androidapp/AppDialogClass.java
+++ app/src/main/java/com/example/androidapp/AppDialogClass.java
@@ -8,15 +8,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class AppDialogClass extends DialogFragment {
 
     private List<String> mSelectedItems;
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
@@ -25,33 +27,36 @@ public class AppDialogClass extends DialogFragment {
         super.onCreate(savedInstanceState);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("<4th> Group’s Dialog")
-                .setMultiChoiceItems(R.array.members, null, new DialogInterface.OnMultiChoiceClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                        String[] items = getActivity().getResources().getStringArray(R.array.members);
+            .setMultiChoiceItems(R.array.members, null, new DialogInterface.OnMultiChoiceClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                    String[] items = Objects.requireNonNull(getActivity()).getResources().getStringArray(R.array.members);
 
-                        if(isChecked){
-                            mSelectedItems.add(items[which]);
-                            Toast.makeText(getActivity(),"<"+items[which]+"> checked", Toast.LENGTH_SHORT).show();
-                        } else if(mSelectedItems.contains(items[which])){
-                            mSelectedItems.remove(items[which]);
-                            Toast.makeText(getActivity(),"<"+items[which]+"> unchecked", Toast.LENGTH_SHORT).show();
-                        }
+                    if(isChecked){
+                        mSelectedItems.add(items[which]);
+                        Toast.makeText(getActivity(),"<"+items[which]+"> checked", Toast.LENGTH_SHORT).show();
+                    } else if(mSelectedItems.contains(items[which])){
+                        mSelectedItems.remove(items[which]);
+                        Toast.makeText(getActivity(),"<"+items[which]+"> unchecked", Toast.LENGTH_SHORT).show();
                     }
-                })
-                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                            //this is getting overrode
-                    }
-                })
-                .setNegativeButton("close", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getActivity(), "You closed dialog", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-        return builder.create();
+                }
+            })
+            .setPositiveButton("ok", null)
+            .setNegativeButton("close", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(getActivity(), "You closed dialog", Toast.LENGTH_SHORT).show();
+                }
+            });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+        positiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "You clicked OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return dialog;
     }
 }
