f2dad6822f36e9080ce85b9a9e54ed1aafcb75d7
Krishoo169 <64309605+Krishoo169@users.noreply.github.com>
2020-12-23 22:15:44 +0200

    Need to add themes

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -1,13 +1,97 @@
 package com.example.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Button buttonScnd;
+    private EditText editText;
+    private Button buttonSave;
+    private Button buttonClear;
+    private EditText showText;
+
+    public static final String SHARED_PREFERENCES = "sharedPreferences";
+    public static final String TEXT = "text";
+
+    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        buttonSave = (Button) findViewById(R.id.buttonSave);
+        showText = (EditText) findViewById(R.id.editTextTextPersonName2);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.themes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+
+        buttonScnd = (Button) findViewById(R.id.buttonGoScnd);
+        buttonScnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+
+        buttonSave.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View v){
+                editText.setText(editText.getText().toString());
+                saveData();
+        }
+        });
+
+        loadData();
+        updateViews();
+
+}
+
+    public void saveData() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(TEXT, editText.getText().toString());
+        editor.apply();
+    }
+
+    public void loadData(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());;
+        text = sharedPreferences.getString(TEXT, "");
+    }
+
+    public void updateViews(){
+        editText.setText(text);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/androidapp/MainActivity2.java
@@ -0,0 +1,60 @@
+package com.example.androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static com.example.androidapp.MainActivity.TEXT;
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button button;
+    private Button buttonReadPrefs;
+    private EditText showPref;
+
+    public static final String TEXT = "text";
+
+    private String text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        showPref = (EditText) findViewById(R.id.editTextTextPersonName2);
+        buttonReadPrefs = (Button) findViewById(R.id.buttonReadPref);
+        button = (Button) findViewById(R.id.buttonBack);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        buttonReadPrefs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadData();
+                updateViews();
+                if(text.length() >= 0){
+                   Toast.makeText(MainActivity2.this, "Nothing found", Toast.LENGTH_SHORT).show();
+                } else {
+                    showPref.setText(text);
+                }
+            }
+        });
+    }
+
+    public void loadData(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        text = sharedPreferences.getString(TEXT, "");
+    }
+
+    public void updateViews() {
+        showPref.setText(text);
+    }
+}
